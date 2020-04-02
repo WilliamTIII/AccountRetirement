40e05aa77cce5870bf5c95235e887236f7bc5838
Steve Tollaksen <steve_tollaksen@SHAMROCKFOODS.com>
2020-04-02 08:09:44 -0700

    added scripts

---
+++ revokemfa_disablemailrules.ps1
@@ -0,0 +1,26 @@
+﻿$file = Import-Csv  "C:\steve\terms.csv"
+Connect-AzureAD
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session -DisableNameChecking
+
+
+
+ForEach ($user in $file)
+{
+    
+    $ID = $user.employeeid
+    $username = Get-adUser -filter {EmployeeID -eq $id} -Properties SamAccountName,employeeid,distinguishedname | ?{$_.distinguishedname -notlike "*internal*"}
+    #Get-AzureADUser -ObjectId steve_tollaksen@shamrockfoods.com
+    "Running " + ($username.userprincipalname)
+    Revoke-AzureADUserAllRefreshToken -ObjectId $username.UserPrincipalName
+   Get-InboxRule -Mailbox $username.UserPrincipalName | Disable-InboxRule -Confirm:$false -Force
+}
+
+
+
+
+
+
+import-csv C:\steve\terms.csv
+
+$user.employeeid
---
+++ sharedmailbox_OoOmessage.ps1
@@ -0,0 +1,23 @@
+﻿$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session -DisableNameChecking
+
+$users = import-csv
+
+
+$nomanager=@()
+foreach($user in $users){
+    try{$manager = (Get-ADUser $user -Properties manager).manager}
+    catch{$nomanager+=$user.UserPrincipalName}
+    
+    Get-Mailbox -Identity $user.UserPrincipalName | Set-Mailbox -Type shared
+    Set-MailboxAutoReplyConfiguration -Identity $user.UserPrincipalName -AutoReplyState Enabled -InternalMessage "SOMETHING HERE" -ExternalMessage "SOMETHING HERE"
+    try{Add-MailboxPermission -Identity $user.UserPrincipalName -User (get-aduser $manager).userprincipalname -AccessRights fullaccess }
+    catch{"couldn't add $user.name because they have no manager"}
+
+}
+$nomanager
+
+
+
+$users = get-aduser asal7286
